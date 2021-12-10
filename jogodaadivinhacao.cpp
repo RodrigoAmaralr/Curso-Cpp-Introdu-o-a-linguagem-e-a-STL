5e7d9500cbee4af582225fa3cc77d545eef03a9c
Rodrigo Micromed <rodrigo.com2@gmail.com>
2021-12-10 13:20:21 -0300

    01-Primeiro programa

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    std::cout << "*************************************" << std::endl;
+    cout << "* Bem-Vindos ao jogo da adivinhacao *" << endl;
+    cout << "*************************************" << endl;
+}
