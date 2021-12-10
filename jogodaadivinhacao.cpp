22875484eeb6ecabddcadf9f488e945f5f31261a
Rodrigo Amaral <rodrigo.com2@gmail.com>
2021-12-10 18:32:51 -0300

    Finalizado o modulo 02

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -1,3 +1,4 @@
+// c++ jogodaadivinhacao.cpp -o prog ; ./prog
 #include <iostream>
 
 using namespace std;    //o marcador de uso das funções padrão de C++
@@ -7,6 +8,34 @@ int main(){
     cout << "* Bem-Vindos ao jogo da adivinhacao *" << endl;
     cout << "*************************************" << endl;
 
-    int numero_secreto = 42;
-    cout << "O numero sereto e: " << numero_secreto << endl;
+    const int NUMERO_SECRETO = 52;
+
+    bool nao_acertou = true;
+    int tentativas = 0;
+    
+    while (nao_acertou){
+        tentativas++;
+        int chute;
+        cout << "Tentativa " << tentativas << endl;
+        cout << "Qual e seu chute: ";
+        cin >> chute;
+
+        bool acertou = chute == NUMERO_SECRETO;
+        bool maior = chute > NUMERO_SECRETO;
+        if(acertou){
+            cout << "Parabens! Voce acertou o numero!" << endl;
+            nao_acertou = false;
+        }
+        else if(maior){
+            cout << "Seu chute " << chute 
+            << " foi MAIOR que o numero secreto" << endl;
+        }
+        else{
+            cout << "Seu chute " << chute 
+            << " foi MENOR que o numero secreto" << endl;
+        }
+    }
+    cout << "Fim de jogo!" << endl;
+    cout << "Voce acertou o numero secreto em " << tentativas 
+            << " tentativas" << endl;
 }
