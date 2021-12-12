e033fc0214a452221ceb0ea154bbf07422b860ec
Rodrigo Amaral <rodrigo.com2@gmail.com>
2021-12-12 09:58:48 -0300

    Finalizado o modulo 03. Adicionando pontuaçao ao jogo.

--- jogodaadivinhacao.cpp
+++ jogodaadivinhacao.cpp
@@ -8,10 +8,12 @@ int main(){
     cout << "* Bem-Vindos ao jogo da adivinhacao *" << endl;
     cout << "*************************************" << endl;
 
-    const int NUMERO_SECRETO = 52;
+    const int NUMERO_SECRETO = 42;
 
     bool nao_acertou = true;
     int tentativas = 0;
+
+    double pontos = 1000.0;
     
     while (nao_acertou){
         tentativas++;
@@ -20,8 +22,13 @@ int main(){
         cout << "Qual e seu chute: ";
         cin >> chute;
 
+        double pontos_perdidos = abs(chute - NUMERO_SECRETO) / 2.0;
+        pontos = pontos - pontos_perdidos;
+
         bool acertou = chute == NUMERO_SECRETO;
         bool maior = chute > NUMERO_SECRETO;
+
+
         if(acertou){
             cout << "Parabens! Voce acertou o numero!" << endl;
             nao_acertou = false;
@@ -38,4 +45,7 @@ int main(){
     cout << "Fim de jogo!" << endl;
     cout << "Voce acertou o numero secreto em " << tentativas 
             << " tentativas" << endl;
+    cout.precision(2);
+    cout << fixed;
+    cout << "Sua pontuação foi de " << pontos << " pontos." << endl;
 }
