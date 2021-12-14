5cfd9a062d9752075154ab22a158c2e6f945614e
Rodrigo Micromed <rodrigo.com2@gmail.com>
2021-12-14 07:58:38 -0300

    Escrevendo a palavra secreta

--- forca.cpp
+++ forca.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
 //g++ .\forca.cpp -o forca.exe ; .\forca.exe -std=c++11
@@ -7,6 +8,7 @@ using namespace std;
 //make forca                linux
 
 const string PALAVRA_SECRETA = "MELANCIA";
+map<char, bool> chutou;
 
 bool letra_exite(char chute){
     for(char letra : PALAVRA_SECRETA){
@@ -18,14 +20,21 @@ bool letra_exite(char chute){
 }
 
 int main(){
-    cout << PALAVRA_SECRETA << endl;
-
     bool nao_acertou = true;
     bool nao_enforcou = true;
 
     while(nao_acertou && nao_enforcou){
+        for(char letra : PALAVRA_SECRETA){
+            if(chutou[letra]){
+                cout << letra << " ";
+            }else{
+                cout << "_ ";
+            }
+        }
         char chute;
         cin >> chute;
+        chutou[chute] = true;
+
         if(letra_exite(chute)){
             cout << "Voce acertou! Seu chute esta na palavra." << endl;
         }else{
