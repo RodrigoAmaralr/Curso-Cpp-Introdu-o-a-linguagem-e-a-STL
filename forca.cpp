da3f5b6eb7ea19fa634f844924eaf31cb740eee9
Rodrigo Micromed <rodrigo.com2@gmail.com>
2021-12-15 08:33:38 -0300

    Finalizado o jogo da forca

--- forca.cpp
+++ forca.cpp
@@ -83,16 +83,23 @@ vector<string> le_arquivo(){
     ifstream arquivo;
     arquivo.open("palavras.txt");
 
-    int quantidade_palavras;
-    arquivo >> quantidade_palavras;
+    if(arquivo.is_open()){
+        int quantidade_palavras;
+        arquivo >> quantidade_palavras;
 
-    vector<string> palavras_do_arquivo;
-    for(int i = 0; i < quantidade_palavras; i++){
-        string palavra_lida;
-        arquivo >> palavra_lida;
-        palavras_do_arquivo.push_back(palavra_lida);
+        vector<string> palavras_do_arquivo;
+        for(int i = 0; i < quantidade_palavras; i++){
+            string palavra_lida;
+            arquivo >> palavra_lida;
+            palavras_do_arquivo.push_back(palavra_lida);
+        }
+        arquivo.close();
+        return palavras_do_arquivo;
     }
-    return palavras_do_arquivo;
+    else{
+        cout << "Nao foi possivel acessar o banco de palavrars. " << endl;
+        exit(0);
+    }
 }
 
 void sorteia_palavra(){
@@ -100,6 +107,31 @@ void sorteia_palavra(){
     srand(time(NULL));
     int indice_sorteado = rand() % palavras.size();
     palavra_secreta = palavras[indice_sorteado];
+}
+
+void salvar_arquivo(vector<string> nova_lista){
+    ofstream arquivo;
+    arquivo.open("palavras.txt");
+    if(arquivo.is_open()){
+        arquivo << nova_lista.size() << endl;
+        for(string palavra : nova_lista){
+            arquivo << palavra << endl;
+        }
+        arquivo.close();
+    }
+    else{
+        cout << "Nao foi possivel acessar o banco de palavrars. " << endl;
+        exit(0);
+    }
+}
+
+void adiciona_palavra(){
+    cout << "Digite a nova palavra, usando letras maiusculas. " << endl;
+    string nova_palavra;
+    cin >> nova_palavra;
+    vector<string> lista_palavras = le_arquivo();
+    lista_palavras.push_back(nova_palavra);
+    salvar_arquivo(lista_palavras);
 }
 
 int main(){
@@ -120,5 +152,12 @@ int main(){
     }
     else{
         cout << "Parabens! Voce acertou a palavrar" << endl;
+
+        cout << "Voce deseja adicionar uma nova palavra ao banco? (S/N) ";
+        char resposta;
+        cin >> resposta;
+        if(resposta == 'S'){
+            adiciona_palavra();
+        }
     }
 }
